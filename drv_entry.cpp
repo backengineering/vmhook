44e181746019d47f2ba97fdf72be899c3b165ab1
_xeroxz <xerox@back.engineering>
2021-06-29 21:47:47 -0700

    added sha1 hook, also added image and log...

--- drv_entry.cpp
+++ drv_entry.cpp
@@ -3,16 +3,24 @@
 // 
 //
 
-#include <ntifs.h>
-#include <intrin.h>
-#include <vmhook.hpp>
+#include "scn.hpp"
+#include "shithook.hpp"
+#include "sha1.hpp"
+#include "md5.hpp"
 
 //
 // game cheat offset flash backs...
 //
 
 #define EAC_VM_HANDLE_OFFSET 0xE93D
+#define EAC_SHA1_OFFSET 0x4C00
+#define EAC_MD5_OFFSET 0x37378
+#define EAC_CRC32_OFFSET 0x27C8C
 #define EAC_IMAGE_BASE 0x140000000
+
+#define DBG_PRINT(format, ...) \
+	DbgPrintEx( DPFLTR_SYSTEM_ID, DPFLTR_ERROR_LEVEL, \
+	"[vmhook-eac [core number = %d]]" format, KeGetCurrentProcessorNumber(), __VA_ARGS__)
 
 //
 // vm handler indexes for READQ...
@@ -26,13 +34,21 @@ u8 readq_idxs[] = { 247, 215, 169, 159, 71, 60, 55, 43, 23 };
 
 u8 readdw_idxs[] = { 218, 180, 179, 178, 163, 137, 92, 22, 12 };
 
+// 
+// vm handler indexes for READB
+//
+
+u8 readb_idxs[] = { 249, 231, 184, 160, 88, 85, 48, 9, 2 };
+
 vm::hook_t* g_vmhook = nullptr;
 vm::handler::table_t* g_vm_table = nullptr;
+u64 __image_base = 0u, __image_size = 0u, __image_clone = 0u;
+inline_hook_t __crc32_hook, __sha1_hook, __md5_hook;
 
 void*
 operator new(
 	u64 size
-)
+	)
 {
 	//
 	// Could have also used ExAllocatePoolZero...
@@ -44,12 +60,32 @@ operator new(
 void
 operator delete
 (
-	void* ptr, 
+	void* ptr,
 	u64 size
-)
+	)
 {
 	UNREFERENCED_PARAMETER(size);
 	ExFreePool(ptr);
+}
+
+__declspec(noinline)
+void hook_sha1(
+	void *data, unsigned int len, void* result
+)
+{
+	sha1_ctx ctx;
+	sha1_init(&ctx);
+
+	if (scn::read_only(__image_base, (u64)data))
+	{
+		DBG_PRINT("sha1 hash data = 0x%p, len = 0x%x, result = 0x%p\n", data, len, result);
+		sha1_update(&ctx, (unsigned char*)__image_clone + (((u64)data) - __image_base), len);
+		sha1_final((unsigned char*)result, &ctx);
+		return;
+	}
+
+	sha1_update(&ctx, (unsigned char*) data, len);
+	sha1_final((unsigned char*)result, &ctx);
 }
 
 void
@@ -65,8 +101,8 @@ image_loaded(
 
 	if (!pid && wcsstr(image_name->Buffer, L"EasyAntiCheat.sys"))
 	{
-		if (g_vmhook && g_vm_table)
-			delete g_vmhook, delete g_vm_table;
+		if (g_vmhook && g_vm_table && __image_clone)
+			delete g_vmhook, delete g_vm_table, ExFreePool((void*)__image_clone);
 
 		//
 		// allocate memory for a g_vmhook, g_vm_table and then zero it...
@@ -123,34 +159,46 @@ image_loaded(
 		auto image_base = reinterpret_cast<u64>(image_info->ImageBase);
 		auto handler_table_ptr = reinterpret_cast<u64*>(image_base + EAC_VM_HANDLE_OFFSET);
 
+		__image_clone = reinterpret_cast<u64>(ExAllocatePool(NonPagedPool, image_info->ImageSize));
+		RtlCopyMemory((void*)__image_clone, (void*)image_base, image_info->ImageSize);
+
 		g_vm_table = new vm::handler::table_t(handler_table_ptr, _edit_entry);
 		g_vmhook = new vm::hook_t(image_base, EAC_IMAGE_BASE, _decrypt_handler, _encrypt_handler, g_vm_table);
+		__image_base = image_base, __image_size = image_info->ImageSize;
+
+		const auto callback = [](vm::registers* regs, u8 handler_idx)
+		{
+			const auto read_addr = reinterpret_cast<u64*>(regs->rbp)[0];
+
+			// shoot the tires right off the virtualized integrity checks in about 2 lines of code...
+			if (scn::read_only(__image_base, read_addr))
+			{
+				DBG_PRINT(" READ(Q/DW/B) EasyAntiCheat.sys+0x%x\n", (read_addr - __image_base));
+				reinterpret_cast<u64*>(regs->rbp)[0] = __image_clone + (read_addr - __image_base);
+			}
+		};
 
 		// install hooks on READQ virtual machine handlers...
 		for (auto idx = 0u; idx < sizeof readq_idxs; ++idx)
-		{
-			g_vm_table->set_callback(readq_idxs[idx],
-				[](vm::registers* regs, u8 handler_idx)
-				{
-					DbgPrint("> READQ, reading address = 0x%p\n", reinterpret_cast<u64*>(regs->rbp)[0]);
-				}
-			);
-		}
+			g_vm_table->set_callback(readq_idxs[idx], callback);
 
+		// install hooks on READDW virtual machine handlers...
 		for (auto idx = 0u; idx < sizeof readdw_idxs; ++idx)
-		{
-			g_vm_table->set_callback(readdw_idxs[idx],
-				[](vm::registers* regs, u8 handler_idx)
-				{
-					DbgPrint("> READDW, reading address = 0x%p\n", reinterpret_cast<u64*>(regs->rbp)[0]);
-				}
-			);
-		}
+			g_vm_table->set_callback(readdw_idxs[idx], callback);
+
+		// install hooks on READB virtual machine handlers...
+		for (auto idx = 0u; idx < sizeof readb_idxs; ++idx)
+			g_vm_table->set_callback(readb_idxs[idx], callback);
 
 		//
 		// hooks all vm handlers and starts callbacks...
 		//
 		g_vmhook->start();
+
+		// hook on sha1...
+		make_inline_hook(&__sha1_hook,
+			reinterpret_cast<void*>(
+				image_base + EAC_SHA1_OFFSET), &hook_sha1, true);
 	}
 }
 
@@ -184,6 +232,6 @@ DriverEntry( // entry called from ZwSwapCert...
 	// This is because ZwSwapCert will cause the system to crash...
 	//
 
-	DbgPrint("> Registering ImageLoad Callbacks...\n");
+	DBG_PRINT("> Registering ImageLoad Callbacks...\n");
 	return PsSetLoadImageNotifyRoutine(&image_loaded);
 }
---
+++ shithook.hpp
@@ -0,0 +1,75 @@
+#pragma once
+#include <ntifs.h>
+
+typedef struct _inline_hook_t
+{
+	unsigned char code[14];
+	unsigned char jmp_code[14];
+
+	void* address;
+	void* hook_address;
+} inline_hook_t, * pinline_hook_t;
+
+void make_inline_hook(pinline_hook_t, void*, void*, bool);
+void enable_inline_hook(pinline_hook_t);
+void disable_inline_hook(pinline_hook_t);
+
+inline void make_inline_hook(pinline_hook_t hook, void* hook_from, void* hook_to, bool install)
+{
+	unsigned char jmp_code[14] =
+	{
+		0xff, 0x25, 0x0, 0x0, 0x0, 0x0,		// jmp    QWORD PTR[rip + 0x0]
+
+		// jmp address...
+		0x0, 0x0, 0x0, 0x0,
+		0x0, 0x0, 0x0, 0x0
+	};
+
+	// save original bytes, and hook related addresses....
+	hook->address = hook_from;
+	hook->hook_address = hook_to;
+	memcpy(hook->code, hook_from, sizeof hook->code);
+
+	// setup hook...
+	memcpy(jmp_code + 6, &hook_to, sizeof hook_to);
+	memcpy(hook->jmp_code, jmp_code, sizeof jmp_code);
+	if (install) enable_inline_hook(hook);
+}
+
+inline void enable_inline_hook(pinline_hook_t hook)
+{
+	{
+		auto cr0 = __readcr0();
+		cr0 &= 0xfffffffffffeffff;
+		__writecr0(cr0);
+		_disable();
+	}
+
+	memcpy(hook->address, hook->jmp_code, sizeof hook->jmp_code);
+
+	{
+		auto cr0 = __readcr0();
+		cr0 |= 0x10000;
+		_enable();
+		__writecr0(cr0);
+	}
+}
+
+inline void disable_inline_hook(pinline_hook_t hook)
+{
+	{
+		auto cr0 = __readcr0();
+		cr0 &= 0xfffffffffffeffff;
+		__writecr0(cr0);
+		_disable();
+	}
+
+	memcpy(hook->address, hook->code, sizeof hook->code);
+
+	{
+		auto cr0 = __readcr0();
+		cr0 |= 0x10000;
+		_enable();
+		__writecr0(cr0);
+	}
+}
